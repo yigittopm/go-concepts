32bc01127e0d87299048b8ba55064068612640ce
Mert <yigittopm@hotmail.com>
2023-06-16 12:07:15 +0300

    read from closed channel added

---
+++ channels/read-from-closed/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	intChan := make(chan int)
+	close(intChan)
+
+	value, ok := <-intChan
+	fmt.Println("value: ", value)
+	fmt.Println("ok: ", ok)
+
+	value, ok = <-intChan
+	fmt.Println("value: ", value)
+	fmt.Println("ok: ", ok)
+
+	value, ok = <-intChan
+	fmt.Println("value: ", value)
+	fmt.Println("ok: ", ok)
+}
