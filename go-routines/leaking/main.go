4d3d73fb2a18f3784e6e4f66a9c42ee0e5a80128
Mert <yigittopm@hotmail.com>
2023-06-22 11:33:40 +0300

    leaking added

---
+++ go-routines/leaking/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	fmt.Println("go routines before work: ", runtime.NumGoroutine())
+
+	go work(nil)
+	timer := time.NewTimer(5 * time.Second)
+
+	for {
+		select {
+		case <-timer.C:
+			fmt.Println("exiting")
+			return
+		default:
+		}
+
+		time.Sleep(time.Second)
+		fmt.Println("number of go routines: ", runtime.NumGoroutine())
+	}
+}
+
+func work(strings <-chan string) {
+	defer fmt.Println("work is done")
+
+	for s := range strings {
+		fmt.Println(s)
+	}
+}
