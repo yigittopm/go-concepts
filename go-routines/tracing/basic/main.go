f006126e7671ff096e00b8a844f5efa26efa16c5
Mert <yigittopm@hotmail.com>
2023-07-04 12:54:06 +0300

    tracing basic added

---
+++ go-routines/tracing/basic/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime/trace"
+	"sync"
+)
+
+func main() {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+	defer trace.Stop()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 30; i++ {
+		wg.Add(1)
+		go func() {
+			t := 0
+			for i := 0; i < 100; i++ {
+				t += 2
+			}
+			fmt.Println("total: ", t)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
