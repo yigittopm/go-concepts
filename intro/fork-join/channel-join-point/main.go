4cd52e5092f29b9c85711b4b2e4389756e3de2e0
Mert <yigittopm@hotmail.com>
2023-07-12 12:20:02 +0300

    fork join- join point added

---
+++ intro/fork-join/channel-join-point/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	done := make(chan struct{})
+
+	go func() {
+		work()
+		done <- struct{}{}
+	}()
+
+	<-done
+	fmt.Println("elapsed", time.Since(now))
+	fmt.Println("done waiting, main exits")
+}
+
+func work() {
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("printing some stuff")
+}
